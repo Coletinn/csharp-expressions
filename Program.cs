dce1e3db52c8dc5aad7a88f99a5028cceec08346
Coletinn <gustavocoleto@hotmail.com>
2021-05-25 13:58:42 -0300

    Predicate

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Globalization;
-using Project.Services;
+using Project.Entities;
 
 
 namespace Project
@@ -10,17 +10,25 @@ namespace Project
     delegate void BinaryNumericOperation(double n1, double n2);
     class Program
     {
-        public static int globalValue = 3;
-
         static void Main(string[] args)
         {
-            double a = 10;
-            double b = 12;
+            List<Product> list = new List<Product>();
 
-            BinaryNumericOperation op = CalculationService.Sum;
-            op += CalculationService.Max;
+            list.Add(new Product("TV", 900));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.90));
 
-            op(a,b);
+            list.RemoveAll(ProductTest);
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.00;
         }
     }
 }
