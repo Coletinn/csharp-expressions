00f9285f0f30ff89c07a634089f85a28d43b5739
Coletinn <gustavocoleto@hotmail.com>
2021-05-25 13:07:51 -0300

    Functional programming and lambda

--- Program.cs
+++ Program.cs
@@ -9,19 +9,23 @@ namespace Project
 {
     class Program
     {
+        public static int globalValue = 3;
+
         static void Main(string[] args)
         {
-            List<Product> list = new List<Product>();
+            int[] vect = new int[] { 3, 4, 5 };
+            ChangeOddValues(vect);
+            Console.WriteLine(string.Join(" ",vect));
+        }
 
-            list.Add(new Product("TV", 900));
-            list.Add(new Product("Notebook", 1200));
-            list.Add(new Product("Tablet", 450));
-
-            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
-
-            foreach(Product p in list)
+        public static void ChangeOddValues(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine(p);
+                if (numbers[i] % 2 != 0)
+                {
+                    numbers[i] += globalValue;
+                }
             }
         }
     }
