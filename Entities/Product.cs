a391ccfd90dbf62141ef4930c97f111b55c3f131
Coletinn <gustavocoleto@hotmail.com>
2021-05-27 14:55:06 -0300

    Exercise

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -7,22 +7,14 @@ namespace Project.Entities
 {
     class Product
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
-        public Category Category { get; set; }
 
-        public override string ToString()
+        public Product(string name, double price)
         {
-            return Id
-                + ", "
-                + Name
-                + ", "
-                + Price.ToString("F2", CultureInfo.InvariantCulture)
-                + ", "
-                + Category.Name
-                + ", "
-                + Category.Tier;
+            Name = name;
+            Price = price;
         }
+        
     }
 }
